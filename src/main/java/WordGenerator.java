c7ff8b9acdbc93de3edf2f90826a059921fbca8b
bealej3 <bealej3@uni.coventry.ac.uk>
2022-05-22 16:11:08 +0100

    random number generated

--- src/main/java/WordGenerator.java
+++ src/main/java/WordGenerator.java
@@ -1,8 +1,32 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+
 public class WordGenerator {
 
-    public static String dailyWord() {
-        String newWord = null;
+    @Value("${wordle.list}")
+    private String wordleList;
 
+    private static final Logger logger = LogManager.getLogger(WordGenerator.class);
+
+
+    public String dailyWord() {
+        String newWord = null;
+        int lines = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(wordleList))) {
+            while (reader.readLine() != null) lines++;
+        } catch ( IOException e) {
+            logger.error("Failed to generate today's wordle word, due to being unable to detect wordleWords.csv");
+        }
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt(lines+1);
 
 
         return newWord;
